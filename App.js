b299bcd06e439170c1e5b5517d4d7193fb43e791
IraMira333 <irinat333@gmail.com>
2023-10-03 17:29:27 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
 import { StyleSheet, Text, View } from "react-native";
 import RegistrationScreen from "./Screens/RegistrationScreen";
 
 export default function App() {
+  useFonts({
+    Roboto: require("./assets/fonts/Roboto-Medium.ttf"),
+  });
   return <RegistrationScreen />;
 }
 
