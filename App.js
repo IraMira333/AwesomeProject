022e7fbeec02117bb99376eda01d07222fbab11d
IraMira333 <irinat333@gmail.com>
2023-10-07 23:53:36 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,44 @@
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
+// import {
+//   useFonts,
+//   Roboto_500Medium,
+//   Roboto_400Regular,
+// } from "@expo-google-fonts/roboto";
 import * as SplashScreen from "expo-splash-screen";
-import AppLoading from "expo-app-loading";
+
 import { StyleSheet, Text, View } from "react-native";
 import RegistrationScreen from "./Screens/RegistrationScreen";
 import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
-    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
-    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  // const [fontsLoaded] = useFonts({
+  //   Roboto_500Medium,
+  //   Roboto_400Regular,
+  // });
+  // if (!fontsLoaded) {
+  //   SplashScreen.hideAsync();
+  // }
+  let [fontsLoaded] = useFonts({
+    RobotoMedium: require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
   });
   if (!fontsLoaded) {
     SplashScreen.hideAsync();
   }
-
   return (
     <>
-      <RegistrationScreen />
-      {/* <LoginScreen /> */}
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
