7cb43b06c21127c4185e814cd45c670009bba65a
IraMira333 <irinat333@gmail.com>
2023-10-09 15:18:13 +0300

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { useFonts } from "expo-font";
 //   Roboto_400Regular,
 // } from "@expo-google-fonts/roboto";
 import * as SplashScreen from "expo-splash-screen";
-
+import { Font } from "expo";
 import { StyleSheet, Text, View } from "react-native";
 import RegistrationScreen from "./src/Screens/RegistrationScreen";
 import LoginScreen from "./src/Screens/LoginScreen";
@@ -19,13 +19,19 @@ export default function App() {
   // if (!fontsLoaded) {
   //   SplashScreen.hideAsync();
   // }
-  const [fontsLoaded] = useFonts({
-    "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
-    "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
-  });
-  if (!fontsLoaded) {
-    SplashScreen.hideAsync();
-  }
+  // const [fontsLoaded] = useFonts({
+  //   "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
+  //   "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
+  // });
+  // if (!fontsLoaded) {
+  //   SplashScreen.hideAsync();
+  // }
+  loadAssets = async () => {
+    await Font.loadAsync({
+      Roboto_Medium: require("./src/assets/fonts/Roboto-Medium.ttf"),
+      Roboto_Regular: require("./src/assets/fonts/Roboto-Regular.ttf"),
+    });
+  };
   return (
     <>
       <RegistrationScreen />
