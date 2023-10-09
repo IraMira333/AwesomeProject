135462bcfe56d3d32f12e827e2e6075f4818fb45
IraMira333 <irinat333@gmail.com>
2023-10-09 23:17:58 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,50 +1,41 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
+
+import { useCallback } from "react";
 import { useFonts } from "expo-font";
-// import {
-//   useFonts,
-//   Roboto_500Medium,
-//   Roboto_400Regular,
-// } from "@expo-google-fonts/roboto";
+
 import * as SplashScreen from "expo-splash-screen";
-import { Font } from "expo";
+
 import { StyleSheet, Text, View } from "react-native";
 import RegistrationScreen from "./src/Screens/RegistrationScreen";
 import LoginScreen from "./src/Screens/LoginScreen";
 
 export default function App() {
-  // const [fontsLoaded] = useFonts({
-  //   Roboto_500Medium,
-  //   Roboto_400Regular,
-  // });
-  // if (!fontsLoaded) {
-  //   SplashScreen.hideAsync();
-  // }
-  // const [fontsLoaded] = useFonts({
-  //   "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
-  //   "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
-  // });
-  // if (!fontsLoaded) {
-  //   SplashScreen.hideAsync();
-  // }
-  loadAssets = async () => {
-    await Font.loadAsync({
-      Roboto_Medium: require("./src/assets/fonts/Roboto-Medium.ttf"),
-      Roboto_Regular: require("./src/assets/fonts/Roboto-Regular.ttf"),
-    });
-  };
+  const [fontsLoaded] = useFonts({
+    "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <>
-      <RegistrationScreen />
-      {/* <LoginScreen /> */}
-    </>
+    <View onLayout={onLayoutRootView}>
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
+    </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
